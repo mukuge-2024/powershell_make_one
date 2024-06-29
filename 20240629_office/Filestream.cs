928f4e73e8a2ed7fb35ece4dbe1161617529705f
mukuge-2024 <172892872+mukuge-2024@users.noreply.github.com>
2024-06-30 05:47:40 +0900

    Update Filestream.cs

--- 20240629_office/Filestream.cs
+++ 20240629_office/Filestream.cs
@@ -1,2 +1,27 @@
 as
+
+ 1: // c#at.cs
+ 2:
+ 3: using System;
+ 4: using System.IO;
+ 5: using System.Text;
+ 6:
+ 7: public class CatSample {
+ 8:   public static void Main(string[] args) {
+ 9:
+10:     string line;
+11:
+12:     foreach (string arg in args) {
+13:       FileStream   fs = new FileStream(arg, FileMode.Open);
+14:       StreamReader sr = new StreamReader(fs, Encoding.GetEncoding(932));
+15:
+16:       while ((line = sr.ReadLine()) != null) {
+17:         Console.WriteLine(line);
+18:       }
+19:       sr.Close();
+20:       fs.Close();
+21:     }
+22:   }
+23: }    
+
   
